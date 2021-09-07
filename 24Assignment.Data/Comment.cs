8e61888545978c24670e24c4c2960975f0f6f7fd
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-07 16:03:07 -0400

    properties in comment table

---
+++ 24Assignment.Data/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Assignment.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        public int PostId { get; set; }
+        
+    }
+}
