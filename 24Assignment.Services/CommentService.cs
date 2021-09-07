5f0d6120535a4d126bff7ec38a5ab5cf9ece5eb4
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-07 18:06:49 -0400

    post method for comment

---
+++ 24Assignment.Services/CommentService.cs
@@ -0,0 +1,25 @@
+﻿using _24Assignment.Data;
+using _24Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Assignment.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateComment(CommentCreate model)
+        {
+            var entity = new Comment() 
+        }
+    }
+}
